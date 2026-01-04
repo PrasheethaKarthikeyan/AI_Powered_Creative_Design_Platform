03be4934e84f331cca45e85261d9f0110c8b248a
Prasheetha Karthikeyan <prasheethakarthikeyan@gmail.com>
2026-01-04 16:37:01 +0000

    more accurate the pupil placement little moved side

--- src/components/opening-animation.tsx
+++ src/components/opening-animation.tsx
@@ -41,17 +41,19 @@ export function OpeningAnimation() {
         </svg>
 
         {/* Iris and Pupil */}
-        <div className="iris-container">
-          <div className="iris">
-            <div className="pupil-v2">
-                <div className="app-logo-container">
-                    <svg className="app-logo" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 4.5C7 4.5 2.73 7.61 1 12C2.73 16.39 7 19.5 12 19.5C17 19.5 21.27 16.39 23 12C21.27 7.61 17 4.5 12 4.5Z" stroke="#A7C7E7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#A7C7E7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg>
-                </div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="iris-container">
+            <div className="iris">
+              <div className="pupil-v2">
+                  <div className="app-logo-container">
+                      <svg className="app-logo" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M12 4.5C7 4.5 2.73 7.61 1 12C2.73 16.39 7 19.5 12 19.5C17 19.5 21.27 16.39 23 12C21.27 7.61 17 4.5 12 4.5Z" stroke="#A7C7E7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                          <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#A7C7E7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                      </svg>
+                  </div>
+              </div>
+              <div className="light-ripple"></div>
             </div>
-            <div className="light-ripple"></div>
           </div>
         </div>
       </div>
